cd280f113355070d1a34c0403b5dca57ef3ff41e
crypto-vincent <vincent.brunet.us@gmail.com>
2026-04-02 19:04:55 +0900

    nit-test

--- tests/unit.fuzzed.alternatives.ts
+++ tests/unit.fuzzed.alternatives.ts
@@ -5,11 +5,19 @@ it("run", async function () {
   expect(
     fuzzedAlternatives("--inst", ["--install", "--flag", "--blah"]),
   ).toStrictEqual(["--install"]);
+
   expect(
     fuzzedAlternatives("instlal", ["install", "dudu", "--blah"]),
   ).toStrictEqual(["install"]);
 
-  expect(fuzzedAlternatives("cat", ["cut"])).toStrictEqual(["cut"]);
-  expect(fuzzedAlternatives("cat", ["cats"])).toStrictEqual(["cats"]);
+  expect(
+    fuzzedAlternatives("cat", ["cats", "catz", "cut", "kat", "hello", "world"]),
+  ).toStrictEqual(["cats", "catz", "cut"]);
+
+  expect(fuzzedAlternatives("cat", ["cut", "kat"])).toStrictEqual([
+    "cut",
+    "kat",
+  ]);
+
   expect(fuzzedAlternatives("acb", ["abc"])).toStrictEqual(["abc"]);
 });
